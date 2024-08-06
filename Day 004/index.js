1734f3ddc745719ad0a4e4c7b1457a00e543cf8b
cryptus-neoxys <sharma.dev4242@gmail.com>
2024-08-06 13:35:19 +0530

    feat: day 4

--- Day 004/index.js
+++ Day 004/index.js
@@ -1,18 +1,18 @@
-function doSomething() {
-  console.log("Hi from doSomething() event handler");
-}
+// function doSomething() {
+//   console.log("Hi from doSomething() event handler");
+// }
 
-const btns = document.getElementsByTagName("button");
+// const btns = document.getElementsByTagName("button");
 
-for (const btn of btns) {
-  btn.addEventListener("click", doSomething);
-}
+// for (const btn of btns) {
+//   btn.addEventListener("click", doSomething);
+// }
 
-function removeEventListeners() {
-  for (const btn of btns) {
-    btn.removeEventListener("click", doSomething);
-  }
-}
+// function removeEventListeners() {
+//   for (const btn of btns) {
+//     btn.removeEventListener("click", doSomething);
+//   }
+// }
 
 // ES6 --> ECMAScript6
 // ES6 feature: arrow functions
@@ -20,13 +20,31 @@ function removeEventListeners() {
 // param => {} when 1 parameter
 // param => <code> when single line of code, which is returned
 
+const postContainer = document.getElementById("post-container");
+
 function getPosts() {
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((res) => {
       res
         .json()
-        .then((body) => {
-          console.log(body);
+        .then((posts) => {
+          console.log(posts);
+          posts.forEach((post) => {
+            // const postDiv = document.createElement("div");
+            // const postTitle = document.createElement("h3");
+            // postTitle.innerText = post.title;
+            // const postBody = document.createElement("p");
+            // postBody.innerText = post.body;
+            // postDiv.appendChild(postTitle);
+            // postDiv.appendChild(postBody);
+            // postContainer.appendChild(postDiv);
+            postContainer.innerHTML += `
+            <div>
+              <h3>${post.title}</h3>
+              <p>${post.body}</p>
+            </div>
+            `;
+          });
         })
         .catch((err) => console.error(err));
     })
